b6dece69524135657ad456a4f7c3286aa890b98b
Yuku Kotani <yukukotani@gmail.com>
2020-12-18 01:04:17 +0900

    handle browsers that not support shadow dom

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 import { html, render } from "uhtml";
 
 const root = document.getElementById("ubie-banner");
-const width = parseInt(root.dataset.width);
-const shadow = root.attachShadow({ mode: "closed" });
 
-const onImageLoad = () => {
-  const node = html.node`
-  Powered by <a href="https://ubie.life" target="_blank" rel="noopener">Ubie, inc.<a/>
+if (root.attachShadow) {
+  const width = parseInt(root.dataset.width);
+  const shadow = root.attachShadow({ mode: "closed" });
+
+  const onImageLoad = () => {
+    const node = html.node`
+    Powered by <a href="https://ubie.life" target="_blank" rel="noopener">Ubie, inc.<a/>
     `;
-  render(shadow.getElementById("corp-link"), node);
-};
+    render(shadow.getElementById("corp-link"), node);
+  };
 
-const style = `
-a, a:link, a:visited, a:hover, a:active {
-  text-decoration: none;
-  color: black;
-}
-.wrapper {
-  width: ${width}px;
-  padding: 8px;
-  border: 1px solid black;
-}
-.app-img {
-  width: inherit;
-}
-.corp-link {
-  text-align: right;
-}
-.corp-link > a {
-  text-decoration: underline;
-  color: #cf4a7b;
-}
+  const style = `
+  a, a:link, a:visited, a:hover, a:active {
+    text-decoration: none;
+    color: black;
+  }
+  .wrapper {
+    width: ${width}px;
+    padding: 8px;
+    border: 1px solid black;
+  }
+  .app-img {
+    width: inherit;
+  }
+  .corp-link {
+    text-align: right;
+  }
+  .corp-link > a {
+    text-decoration: underline;
+    color: #cf4a7b;
+  }
   `;
 
-const node = html.node`
+  const node = html.node`
   <style>${style}</style>
   <a class="app-link" href="https://ubie.app" target="_blank" rel="noopener">
     <div class="wrapper">
@@ -43,4 +45,5 @@ const node = html.node`
   </a>
   `;
 
-render(shadow, node);
+  render(shadow, node);
+}
